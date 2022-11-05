84cbadacfe7cea321a4138955a67c460f90973e5
Alexander Piskun <bigcat88@icloud.com>
2022-11-05 15:07:11 +0300

    dev [ci skip]

--- cloud_py_api/misc.py
+++ cloud_py_api/misc.py
@@ -1,26 +1,24 @@
 import sys
 
+import numpy
+import pg8000
+
 # import pi_heif
-# import PIL
+import PIL
 import pymysql
+import pywt
+import scipy
 
 from cloud_py_api._version import __version__
-
-# import numpy
-# import pg8000
-
-
-# import pywt
-# import scipy
 
 
 def print_versions():
     print("Python: ", sys.version)
     print("CloudPyApi:", __version__)
-    # print("pg8000: ", pg8000.__version__)
+    print("pg8000: ", pg8000.__version__)
     print("pymysql: ", pymysql.__version__)
-    # print("pillow: ", PIL.__version__)
+    print("pillow: ", PIL.__version__)
     # print("pi_heif: ", pi_heif.__version__)
-    # print("numpy: ", numpy.__version__)
-    # print("scipy: ", scipy.__version__)
-    # print("pywavelets: ", pywt.__version__)
+    print("numpy: ", numpy.__version__)
+    print("scipy: ", scipy.__version__)
+    print("pywavelets: ", pywt.__version__)
