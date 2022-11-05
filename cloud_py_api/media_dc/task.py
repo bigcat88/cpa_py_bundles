bd00ce35a803376bcef0fe8b2a79ccddf23b3f5e
Alexander Piskun <bigcat88@icloud.com>
2022-11-05 12:43:47 +0300

    dev [ci skip]

--- cloud_py_api/media_dc/task.py
+++ cloud_py_api/media_dc/task.py
@@ -1,9 +1,10 @@
-import numpy
-import scipy
+# import numpy
+# import scipy
 
 
 def process_task(task_info: dict, forced: bool):
     """Top Level function. Checks if we can work on task, and if so - start to process it. Called from `main`."""
     # print(f"Processing task: id={task_info['id']}, forced={forced}")
-    print(numpy.__version__)
-    print(scipy.__version__)
+    # print(numpy.__version__)
+    # print(scipy.__version__)
+    print("hello")
